5403af153365323026871a6199569e356dfc205b
Abhijeet Gurle <abhijeetgurle@gmail.com>
2019-05-10 05:29:34 +0530

    started second assignment

---
+++ assignment-2/todolist/models.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Task(models.Model):
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	title = models.TextField()
+
+	
+	def __str__(self):
+		return self.title
