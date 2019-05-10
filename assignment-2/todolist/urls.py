bef22b5a0041368fdcfdcead3c4e6587c0618e85
Abhijeet Gurle <abhijeetgurle@gmail.com>
2019-05-10 11:43:50 +0530

    Completed login/signup functionality

--- assignment-2/todolist/urls.py
+++ assignment-2/todolist/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show_tasks, name='show_tasks'),
+	path('', views.login_user, name='login_user'),
+	path('logout_user', views.logout_user, name='logout_user'),
+	path('signup', views.signup, name='signup'),
+    path('index', views.show_tasks, name='show_tasks'),
 ]
--- assignment-2/todolist/views.py
+++ assignment-2/todolist/views.py
@@ -1,5 +1,48 @@
 from django.shortcuts import render
+from django.http import *
+from django.contrib.auth import authenticate, login, logout
+from .models import Task
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def login_user(request):
+	username = password = ''
+	if request.POST:
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			if user.is_active:
+				login(request, user)
+				return HttpResponseRedirect('/index')
+
+	return render(request, 'todolist/login.html', {})
+
+
+def logout_user(request):
+	logout(request)
+	return HttpResponseRedirect('/')
+
+
+def signup(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			raw_password = form.cleaned_data.get('password1')
+			user = authenticate(username=username, password=raw_password)
+			login(request, user)
+			return HttpResponseRedirect('/index')
+	else:
+		form = UserCreationForm()
+	return render(request, 'todolist/signup.html', {'form': form})
+
+
 def show_tasks(request):
-    return render(request, 'todolist/index.html', {})
+
+	if request.user.is_authenticated:
+		tasks = Task.objects.filter(author = request.user)
+		return render(request, 'todolist/index.html', {'tasks': tasks, 'current_user': request.user})
+	else:
+		return HttpResponseRedirect('/')	
